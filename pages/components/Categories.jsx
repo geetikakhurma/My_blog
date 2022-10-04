0f2cec298dcda1227c8dafb9b37405985d86ef32
geetikakhurma <geetikakhurma99@gmail.com>
2022-10-04 22:16:33 +0530

    first commit

---
+++ pages/components/Categories.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { getCategories } from '../../services';
+
+const Categories = () => {
+  const [categories , setCategories]=useState([]);
+
+useEffect(()=>{
+    getCategories().then((newCategotries)=> setCategories(newCategotries))
+},[])
+
+  return (
+    <div className='bg-white shadow-lg p-8 mb-8 pb-6 rounded-lg'>
+      <h3 className='text-xl font-semibold border-b pb-4' >  Categories</h3>
+      {categories.map((category, index) => (
+        <Link key={index} href={`/category/${category.slug}`}>
+          <span className="block p-3 mt-2 cursor-pointer">{category.name}</span>
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+export default Categories
---
+++ pages/post/[slug].js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader } from '../components'
+
+import { getPosts , getPostDetails} from '../../services'
+
+const PostDetails = ({post}) => {
+  return (
+    <>
+    <div className="container mx-auto px-10 mb-8">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="col-span-1 lg:col-span-8">
+        <PostDetail post={post} />
+         
+          
+            <CommentsForm slug={post.slug} />
+            <Comments slug={post.slug} />
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+          <PostWidget/>
+          <Categories/>
+          </div>
+        </div>
+    </div>
+    </div>
+
+</>
+  )
+}
+
+export default PostDetails  ;
+
+export async function getStaticProps({ params }) {
+    const data = await getPostDetails(params.slug);
+    return {
+      props: {
+        post: data,
+      },
+    };
+  }
+  
+  export async function getStaticPaths() {
+    const posts = await getPosts();
+    return {
+      paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
+      fallback: false,
+    };
+  }
+  
